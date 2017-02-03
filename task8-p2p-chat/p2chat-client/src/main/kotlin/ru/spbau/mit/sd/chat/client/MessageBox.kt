abfface24baec82c7fb03f6dbdd950485c6d6c54
Egor Gorbunov <egor-mailbox@ya.ru>
2017-02-03 20:19:35 +0300

    Fix dependencies; Add logback logger (I guess)

--- task8-p2p-chat/p2chat-client/src/main/kotlin/ru/spbau/mit/sd/chat/client/MessageBox.kt
+++ task8-p2p-chat/p2chat-client/src/main/kotlin/ru/spbau/mit/sd/chat/client/MessageBox.kt
@@ -1,7 +1,25 @@
 package ru.spbau.mit.sd.chat.client
 
+import ru.spbau.mit.sd.commons.proto.ChatMessage
+import ru.spbau.mit.sd.commons.proto.ChatUserInfo
+import java.util.*
+
+
 /**
- * Structure with all messages for particular client (all his/her conversations)
+ * Structure with all messages for particular client (that is one chat room)
  */
-class MessageBox {
+class MessageBox(val user: ChatUserInfo) {
+    private val messages = ArrayList<Pair<ChatUserInfo, ChatMessage>>()
+
+    fun addMessage(from: ChatUserInfo, message: ChatMessage) {
+        messages.add(Pair(from, message))
+    }
+
+    fun getLastMessages(num: Int): List<Pair<ChatUserInfo, ChatMessage>> {
+        return messages.slice(messages.lastIndex-num..messages.lastIndex)
+    }
+
+    fun getAllMessages(): List<Pair<ChatUserInfo, ChatMessage>>? {
+        return Collections.unmodifiableList(messages)
+    }
 }
