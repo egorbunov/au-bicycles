168512a0e16a5d44c9bf4161aeda5f3da0b36ca0
Egor Gorbunov <egor-mailbox@ya.ru>
2017-02-08 01:12:06 +0300

    Add client-side chat model; Add few exceptions

--- task8-p2p-chat/p2chat-client/src/main/kotlin/ru/mir/spbau/sd/chat/client/model/ChatModel.kt
+++ task8-p2p-chat/p2chat-client/src/main/kotlin/ru/mir/spbau/sd/chat/client/model/ChatModel.kt
@@ -1,9 +1,119 @@
 package ru.mir.spbau.sd.chat.client.model
 
+import ru.mir.spbau.sd.chat.client.ChatUserAlreadyExists
+import ru.mir.spbau.sd.chat.client.ChatUserDoesNotExists
+import ru.spbau.mit.sd.commons.proto.ChatMessage
+import ru.spbau.mit.sd.commons.proto.ChatUserInfo
+import java.util.*
+
 /**
- * Created by: Egor Gorbunov
- * Date: 2/6/17
- * Email: egor-mailbox@ya.com
+ * Chat data holder.
+ * Chat data contains:
+ *     1. Map of online users
+ *     2. Message box for each online user
  */
-class ChatModel {
+class ChatModel<T>(
+        private val clientId: T,
+        private var clientInfo: ChatUserInfo,
+        private val usersMap: AbstractMap<T, ChatUserInfo>
+) {
+    private val usersMsgBoxes = HashMap<T, MutableList<ChatMessage>>()
+    private val listeners = ArrayList<ChatModelChangeListener<T>>()
+
+    private fun createEmptyMsgBox(): MutableList<ChatMessage> {
+        return ArrayList()
+    }
+
+    init {
+        for ((userId) in usersMap) {
+            usersMsgBoxes[userId] = createEmptyMsgBox()
+        }
+    }
+
+    /**
+     * Add new user to client chat
+     */
+    fun addUser(userId: T, userInfo: ChatUserInfo) {
+        if (userId in usersMap) {
+            throw ChatUserAlreadyExists("$userId: ${usersMap[userId]}")
+        }
+
+        usersMap[userId] = userInfo
+        usersMsgBoxes[userId] = createEmptyMsgBox()
+        listeners.forEach { it.newUserAdded(userId, userInfo) }
+    }
+
+    /**
+     * Remove user from client chat
+     */
+    fun removeUser(userId: T) {
+        if (userId !in usersMap) {
+            throw ChatUserDoesNotExists("$userId")
+        }
+
+        usersMap.remove(userId)
+        listeners.forEach { it.userRemoved(userId) }
+    }
+
+    /**
+     * Edit client chat user info
+     */
+    fun editUser(userId: T, newUserInfo: ChatUserInfo) {
+        if (userId !in usersMap) {
+            throw ChatUserDoesNotExists("$userId")
+        }
+
+        usersMap[userId] = newUserInfo
+        listeners.forEach { it.userChanged(userId, newUserInfo) }
+    }
+
+    /**
+     * Get all chat users (except current one)
+     */
+    fun getUsers(): List<Pair<T, ChatUserInfo>> {
+        return usersMap.toList()
+    }
+
+    /**
+     * "Sent" message to recipient
+     */
+    fun addMessageTo(recipient: T, message: ChatMessage) {
+        if (recipient !in usersMap) {
+            throw ChatUserDoesNotExists("$recipient")
+        }
+        usersMsgBoxes[recipient]!!.add(message)
+        listeners.forEach { it.messageSent(recipient, message) }
+    }
+
+    /**
+     * "Receive" message from sender
+     */
+    fun addMessageFrom(sender: T, message: ChatMessage) {
+        if (sender !in usersMap) {
+            throw ChatUserDoesNotExists("$sender")
+        }
+        usersMsgBoxes[sender]!!.add(message)
+        listeners.forEach { it.messageReceived(sender, message) }
+    }
+
+    /**
+     * Get message history with given recipient/sender
+     */
+    fun getMessages(recepient: T): List<ChatMessage> {
+        if (recepient !in usersMap) {
+            throw ChatUserDoesNotExists("$recepient")
+        }
+        return usersMsgBoxes[recepient]!!
+    }
+
+    /**
+     * Change current client information
+     */
+    fun changeClientInfo(newInfo: ChatUserInfo) {
+        clientInfo = newInfo
+        listeners.forEach { it.currentClientInfoChanged(newInfo) }
+    }
+
+    fun addListener(listener: ChatModelChangeListener<T>) = listeners.add(listener)
+    fun removeListener(listener: ChatModelChangeListener<T>) = listeners.remove(listener)
 }
--- task8-p2p-chat/p2chat-client/src/main/kotlin/ru/mir/spbau/sd/chat/client/model/ChatModelChangeListener.kt
+++ task8-p2p-chat/p2chat-client/src/main/kotlin/ru/mir/spbau/sd/chat/client/model/ChatModelChangeListener.kt
@@ -1,9 +1,42 @@
 package ru.mir.spbau.sd.chat.client.model
 
+import ru.spbau.mit.sd.commons.proto.ChatMessage
+import ru.spbau.mit.sd.commons.proto.ChatUserInfo
+
 /**
- * Created by: Egor Gorbunov
- * Date: 2/6/17
- * Email: egor-mailbox@ya.com
+ * Model listener interface, which is triggered by `ChatModel`
  */
-interface ChatModelChangeListener {
+interface ChatModelChangeListener <T> {
+    /**
+     * Triggered in case new user added to chat model; This event
+     * has meaning: user become online
+     */
+    fun newUserAdded(userId: T, userInfo: ChatUserInfo)
+
+    /**
+     * Triggered in case user gone offline (deleted from model)
+     */
+    fun userRemoved(userId: T)
+
+    /**
+     * Triggered if user information has changed
+     */
+    fun userChanged(userId: T, newInfo: ChatUserInfo)
+
+    /**
+     * Triggered in case new message was added to message box from sender
+     * side (it was received!)
+     */
+    fun messageReceived(senderId: T, message: ChatMessage)
+
+    /**
+     * Triggered in case new message was added by current client, so it
+     * was sent
+     */
+    fun messageSent(recipientId: T, message: ChatMessage)
+
+    /**
+     * This client info change trigger
+     */
+    fun currentClientInfoChanged(newInfo: ChatUserInfo)
 }
